bdf59633051ed7cf62e55819a21925b2e39106e1
Ilya <ilapysnyj045@gmail.com>
2025-05-27 17:16:47 +0300

    изменение для сервера на фронтэнде

--- src/shop/Katalog/Items.js
+++ src/shop/Katalog/Items.js
@@ -3,28 +3,30 @@ import axios from "axios";
 
 const usePlants = () => {
   const [plants, setPlants] = useState([]);
-  const [loading, setLoading] = useState(true); // Состояние для отслеживания загрузки
-  const [error, setError] = useState(null); // Состояние для хранения ошибок
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     const fetchPlants = async () => {
       try {
-        const response = await axios.get("http://localhost:5000/plants");
+        const response = await axios.get(
+          "https://bonzaishop-server.onrender.com/plants"
+        );
         setPlants(response.data);
       } catch (error) {
         console.error("Ошибка при получении данных:", error);
         setError(
           "Не удалось загрузить растения. Пожалуйста, попробуйте позже."
-        ); // Устанавливаем сообщение об ошибке
+        );
       } finally {
-        setLoading(false); // Устанавливаем состояние загрузки в false после завершения
+        setLoading(false);
       }
     };
 
     fetchPlants();
   }, []);
 
-  return { plants, loading, error }; // Возвращаем объекты с данными, состоянием загрузки и ошибками
+  return { plants, loading, error };
 };
 
 export default usePlants;
